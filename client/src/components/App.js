65e5b3ac3d4095ca56f212c2e43f5c7556de6d97
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2020-06-02 19:09:45 -0400

    Move routes from App.js to separate file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,31 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { createCart, getCart } from "../_actions/cart_actions";
 import { setIndex } from "../_actions/playlist_actions";
-import Auth from "../hoc/auth";
 // pages for this product
-import LandingPage from "./views/LandingPage/LandingPage.js";
-import LoginPage from "./views/LoginPage/LoginPage.js";
-import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import Main from "./views/Main/Main";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
-import UploadBeatPage from "./views/UploadBeatPage/UploadBeatPage";
-import OrdersPage from "./views/OrdersPage/OrdersPage";
-import DashboardPage from "./views/DashboardPage/DashboardPage";
-import CartPage from "./views/CartPage/CartPage";
-import BeatPage from "./views/BeatPage/BeatPage";
 import BeatPlayer from "./views/BeatPlayer/BeatPlayer";
-import ErrorNotFoundPage from "./views/ErrorNotFoundPage/ErrorNotFoundPage";
 import { AudioContextProvider } from "./utils/AudioContext";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
-
-// Auth(SpecificComponent, option, adminRoute)
-// option:
-const PUBLIC_PAGE = 0;
-const LOGGED_IN_ONLY = 1;
-const PUBLIC_ONLY = 2;
-// adminRoute:
-// true for Admin only
-// false for all users
 
 function App() {
   const dispatch = useDispatch();
@@ -63,23 +45,13 @@ function App() {
     <ThemeProvider>
       <CSSReset />
       <Suspense fallback={(<div>Loading...</div>)}>
-       <AudioContextProvider>
-        <NavBar>
-          <Switch>
-            <Route exact path="/" component={Auth(LandingPage, PUBLIC_PAGE, false)} />
-            <Route exact path="/login" component={Auth(LoginPage, PUBLIC_ONLY, false)} />
-            <Route exact path="/register" component={Auth(RegisterPage, PUBLIC_ONLY, false)} />
-            <Route exact path="/upload" component={Auth(UploadBeatPage, LOGGED_IN_ONLY, true)} />
-            <Route exact path="/beat/:beatUrl" component={Auth(BeatPage, PUBLIC_PAGE, false)} />
-            <Route exact path="/cart" component={Auth(CartPage, PUBLIC_PAGE, false)} />
-            <Route exact path="/orders" component={Auth(OrdersPage, LOGGED_IN_ONLY, false)} />
-            <Route exact path="/dashboard" component={Auth(DashboardPage, LOGGED_IN_ONLY, true)} />
-            <Route component={Auth(ErrorNotFoundPage, PUBLIC_PAGE, false)} />
-          </Switch>
-        </NavBar>
-        <Footer />
-        <BeatPlayer />
-       </AudioContextProvider>
+        <AudioContextProvider>
+          <NavBar>
+            <Main />
+          </NavBar>
+          <Footer />
+          <BeatPlayer />
+        </AudioContextProvider>
       </Suspense>
     </ThemeProvider>
   );
