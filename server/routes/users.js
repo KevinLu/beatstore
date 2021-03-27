77dbe1a570898c9865785a163d31b30b126570fa
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2021-03-26 21:00:09 -0400

    Add fullname to auth response and remove unused endpoints

--- server/routes/users.js
+++ server/routes/users.js
@@ -17,6 +17,7 @@ router.get("/auth", auth, async (req, res) => {
                 isAuth: true,
                 email: req.user.email,
                 username: req.user.username,
+                fullname: req.user.fullname,
                 role: req.user.role,
                 image: req.user.image,
                 cart: req.user.cart,
@@ -45,19 +46,6 @@ router.post("/register", (req, res) => {
     });
 });
 
-router.post("/registerAnon", (req, res) => {
-
-    const user = new User(req.body);
-
-    user.save((err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-            success: true,
-            userId: user._id
-        });
-    });
-});
-
 router.post("/login", (req, res) => {
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -76,33 +64,6 @@ router.post("/login", (req, res) => {
                 if (err) return res.status(400).send(err);
                 res
                     .cookie("w_auth", user.token, { sameSite: "lax", httpOnly: true })
-                    .status(200)
-                    .json({
-                        loginSuccess: true, userId: user._id
-                    });
-            });
-        });
-    });
-});
-
-router.post("/loginAnon", (req, res) => {
-    User.findOne({ _id: req.body.id }, (err, user) => {
-        if (!user) {
-            return res.json({
-                loginSuccess: false,
-                message: "Auth failed, id not found"
-            });
-        }
-
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch) {
-                return res.json({ loginSuccess: false, message: "Wrong password" });
-            }
-
-            user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
-                res
-                    .cookie("w_auth", user.token)
                     .status(200)
                     .json({
                         loginSuccess: true, userId: user._id
